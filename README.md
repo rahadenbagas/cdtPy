f1a832db88813245019eb92f68485019fc4710c0
Rahaden B. Hatmaja <116053577+rahadenbagas@users.noreply.github.com>
2023-03-28 02:06:38 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Climate Data Toolbox (CDT) for Python
+
+A simple Python package for analyzing of climate-related disciplines of Earth science data
+
+## Installation
+
+pip install cdtPy
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cdtPy',
+    version='0.1.0',
+    author='R.B. Hatmaja',
+    author_email='rahadenbagas@gmail.com',
+    description='A simple Python package for analyzing of climate-related disciplines of Earth science data',
+    packages=find_packages(),
+    install_requires=[],
+)
